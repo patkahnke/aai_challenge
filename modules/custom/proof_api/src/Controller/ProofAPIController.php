531a603181e4bb6510cad4a6b01677342a1a2edd
Patrick Kahnke <patkahnke@gmail.com>
2016-08-28 02:08:00 -0500

    theme experiment

--- modules/custom/proof_api/src/Controller/ProofAPIController.php
+++ modules/custom/proof_api/src/Controller/ProofAPIController.php
@@ -20,26 +20,26 @@ class ProofAPIController extends ControllerBase
     {
         $response = $this->proofAPIRequests->listAllMovies();
 
-        $json = json_decode($response, true);
-        $dataArray = $json['data'];
-        $createdAt = array();
+//        $json = json_decode($response, true);
+//        $dataArray = $json['data'];
+//        $createdAt = array();
+//
+//        foreach ($dataArray as $movie) {
+//            $createdAt[] = $movie['attributes']['created_at'];
+//        }
+//
+//        array_multisort($createdAt, SORT_DESC, $dataArray);
+//
+//        print "<table>";
+//
+//            for ($i = 0; $i < count($dataArray); $i++) {
+//
+//                print "<tr><td><a href='" . $dataArray[$i]['attributes']['url'] . "'>" . $dataArray[$i]['attributes']['title'] . "</td>";
+//            }
+//
+//            print "</table>";
 
-        foreach ($dataArray as $movie) {
-            $createdAt[] = $movie['attributes']['created_at'];
-        }
-
-        array_multisort($createdAt, SORT_DESC, $dataArray);
-
-        print "<table>";
-
-            for ($i = 0; $i < count($dataArray); $i++) {
-
-                print "<tr><td><a href='" . $dataArray[$i]['attributes']['url'] . "'>" . $dataArray[$i]['attributes']['title'] . "</td>";
-            }
-
-            print "</table>";
-
-        return new Response();
+        return new Response($response);
 
     }
 
@@ -74,27 +74,27 @@ class ProofAPIController extends ControllerBase
     {
         $response = $this->proofAPIRequests->listTopTenByVotes();
 
-        $json = json_decode($response, true);
-        $dataArray = $json['data'];
-        $voteTally = array();
+//        $json = json_decode($response, true);
+//        $dataArray = $json['data'];
+//        $voteTally = array();
+//
+//        foreach ($dataArray as $movie) {
+//            $voteTally[] = $movie['attributes']['vote_tally'];
+//        }
+//
+//        array_multisort($voteTally, SORT_DESC, $dataArray);
+//
+//        print "<table>";
+//
+//        for ($i = 0; $i < count($dataArray) && $i < 10; $i++) {
+//
+//            print "<tr><td>" . ($i + 1) . ")</td><td>" . "<a href='" . $dataArray[$i]['attributes']['url'] . "'>" .
+//                $dataArray[$i]['attributes']['title'] . "</td><td>" . $dataArray[$i]['attributes']['vote_tally'] . " votes</td>";
+//        }
+//
+//        print "</table>";
 
-        foreach ($dataArray as $movie) {
-            $voteTally[] = $movie['attributes']['vote_tally'];
-        }
-
-        array_multisort($voteTally, SORT_DESC, $dataArray);
-
-        print "<table>";
-
-        for ($i = 0; $i < count($dataArray) && $i < 10; $i++) {
-
-            print "<tr><td>" . ($i + 1) . ")</td><td>" . "<a href='" . $dataArray[$i]['attributes']['url'] . "'>" .
-                $dataArray[$i]['attributes']['title'] . "</td><td>" . $dataArray[$i]['attributes']['vote_tally'] . " votes</td>";
-        }
-
-        print "</table>";
-
-        return new Response();
+        return new Response($response);
     }
 
     public function newMovie()
