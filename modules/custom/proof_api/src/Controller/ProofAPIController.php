590a3d122b8ee7ac2e29ff80630b3f007c4c8406
Patrick Kahnke <patkahnke@gmail.com>
2016-08-31 16:30:04 -0500

    restrist new videos to weekdays

--- modules/custom/proof_api/src/Controller/ProofAPIController.php
+++ modules/custom/proof_api/src/Controller/ProofAPIController.php
@@ -9,124 +9,142 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ProofAPIController extends ControllerBase
 {
-    private $proofAPIRequests;
+  private $proofAPIRequests;
 
-    public function __construct(ProofAPIRequests $proofAPIRequests)
-    {
-        $this->proofAPIRequests = $proofAPIRequests;
+  public function __construct(ProofAPIRequests $proofAPIRequests)
+  {
+      $this->proofAPIRequests = $proofAPIRequests;
+  }
+
+  /**
+   * @return array
+   */
+
+  public function allVideos()
+  {
+    $response = $this->proofAPIRequests->listAllVideos();
+
+    $json = json_decode($response, true);
+    $dataArray = $json['data'];
+    $createdAt = array();
+
+    foreach ($dataArray as $movie) {
+        $createdAt[] = $movie['attributes']['created_at'];
     }
 
-    /**
-     * @return array
-     */
-    public function allVideos()
-    {
-        $response = $this->proofAPIRequests->listAllVideos();
+    array_multisort($createdAt, SORT_DESC, $dataArray);
 
-        $json = json_decode($response, true);
-        $dataArray = $json['data'];
-        $createdAt = array();
+    $page = array(
+        '#theme' => 'movies',
+        '#movies' => $dataArray,
+        '#redirectTo' => 'proof_api.all_videos',
+        '#cache' => array
+        (
+            'max-age' => 0,
+        ),
+    );
 
-        foreach ($dataArray as $movie) {
-            $createdAt[] = $movie['attributes']['created_at'];
-        }
+    return $page;
 
-        array_multisort($createdAt, SORT_DESC, $dataArray);
+  }
 
-        $page = array(
-            '#theme' => 'movies',
-            '#movies' => $dataArray,
-            '#redirectTo' => 'proof_api.all_videos',
-        );
-        
-        return $page;
+  public function topTenByViews()
+  {
+    $response = $this->proofAPIRequests->listTopTenByViews();
 
+    $json = json_decode($response, true);
+    $dataArray = $json['data'];
+    $viewTally = array();
+
+    foreach ($dataArray as $movie) {
+        $viewTally[] = $movie['attributes']['view_tally'];
     }
 
-    public function topTenByViews()
-    {
-        $response = $this->proofAPIRequests->listTopTenByViews();
+    array_multisort($viewTally, SORT_DESC, $dataArray);
+    $dataArray = array_slice($dataArray, 0, 10, true);
 
-        $json = json_decode($response, true);
-        $dataArray = $json['data'];
-        $viewTally = array();
+    $page = array(
+        '#theme' => 'movies',
+        '#movies' => $dataArray,
+        '#redirectTo' => 'proof_api.top_ten_by_views',
+        '#cache' => array
+        (
+            'max-age' => 300,
+        ),
+    );
 
-        foreach ($dataArray as $movie) {
-            $viewTally[] = $movie['attributes']['view_tally'];
-        }
+    return $page;
+  }
 
-        array_multisort($viewTally, SORT_DESC, $dataArray);
-        $dataArray = array_slice($dataArray, 0, 10, true);
+  public function topTenByVotes()
+  {
+    $response = $this->proofAPIRequests->listTopTenByVotes();
 
-        $page = array(
-            '#theme' => 'movies',
-            '#movies' => $dataArray,
-            '#redirectTo' => 'proof_api.top_ten_by_views',
-        );
+    $json = json_decode($response, true);
+    $dataArray = $json['data'];
+    $voteTally = array();
 
-        return $page;
+    foreach ($dataArray as $movie) {
+        $voteTally[] = $movie['attributes']['vote_tally'];
     }
 
-    public function topTenByVotes()
+    array_multisort($voteTally, SORT_DESC, $dataArray);
+    $dataArray = array_slice($dataArray, 0, 10, true);
+
+    $page = array(
+        '#theme' => 'movies',
+        '#movies' => $dataArray,
+        '#redirectTo' => 'proof_api.top_ten_by_votes',
+        '#cache' => array
+        (
+            'max-age' => 300,
+        ),
+    );
+
+    return $page;
+  }
+
+  public function newMovie()
+  {
+    if (date('N') < 1)
     {
-        $response = $this->proofAPIRequests->listTopTenByVotes();
-
-        $json = json_decode($response, true);
-        $dataArray = $json['data'];
-        $voteTally = array();
-
-        foreach ($dataArray as $movie) {
-            $voteTally[] = $movie['attributes']['vote_tally'];
-        }
-
-        array_multisort($voteTally, SORT_DESC, $dataArray);
-        $dataArray = array_slice($dataArray, 0, 10, true);
-
-        $page = array(
-            '#theme' => 'movies',
-            '#movies' => $dataArray,
-            '#redirectTo' => 'proof_api.top_ten_by_votes',
-        );
-
-        return $page;
+      return $this->redirect('proof_api.new_movie_form');
+    } else {
+      echo "Sorry - you cannot add new videos on the weekend.";
     }
+  }
 
-    public function newMovie()
-    {
-        return $this->redirect('proof_api.new_movie_form');
-    }
+  public function voteUp($videoID, $redirectTo)
+  {
+    $this->proofAPIRequests->postNewVoteUp($videoID);
 
-    public function voteUp($videoID, $redirectTo)
-    {
-        $this->proofAPIRequests->postNewVoteUp($videoID);
+    return $this->redirect($redirectTo);
+  }
 
-        return $this->redirect($redirectTo);
-    }
+  public function voteDown($videoID, $redirectTo)
+  {
+    $this->proofAPIRequests->postNewVoteDown($videoID);
 
-    public function voteDown($videoID, $redirectTo)
-    {
-        $this->proofAPIRequests->postNewVoteDown($videoID);
+    return $this->redirect($redirectTo);
 
-        return $this->redirect($redirectTo);
+  }
 
-    }
+  public function viewMovie($videoID)
+  {
 
-    public function viewMovie($videoID)
-    {
+    $response = $this->proofAPIRequests->getVideo($videoID);
+    $json = json_decode($response, true);
+    $url = $json['data']['attributes']['url'];
 
-        $response = $this->proofAPIRequests->getVideo($videoID);
-        $json = json_decode($response, true);
-        $url = $json['data']['attributes']['url'];
+    return new TrustedRedirectResponse($url);
+  }
 
-        return new TrustedRedirectResponse($url);
-    }
+  public static function create(ContainerInterface $container)
+  {
+    $proofAPIRequests = $container->get('proof_api.proof_api_requests');
 
-    public static function create(ContainerInterface $container)
-    {
-        $proofAPIRequests = $container->get('proof_api.proof_api_requests');
-
-        return new static($proofAPIRequests);
-    }
+    return new static($proofAPIRequests);
+  }
 
 
 }
